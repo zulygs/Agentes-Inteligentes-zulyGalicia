06bae5baca07f811f14a99ebd595f047ee8e27c2
zulygs <galiciaz600@gmail.com>
2025-03-01 12:14:48 -0600

    Inicio de programacion problema 3: Agente de Navegacion Autonoma

---
+++ agenteNavegacionAutonoma.py
@@ -0,0 +1,10 @@
+from collections import deque 
+
+# asi hacemos  la matriz de 5*5
+laberinto = [
+    ['A', 'O', 'X', 'O', 'X'], 
+    ['O', 'O', 'X', 'O', 'X'],   
+    ['O', 'O', 'O', 'O', 'X'],  
+    ['X', 'X', 'X', 'O', 'X'], 
+    ['O', 'O', 'O', 'O', 'Meta']    
+]
